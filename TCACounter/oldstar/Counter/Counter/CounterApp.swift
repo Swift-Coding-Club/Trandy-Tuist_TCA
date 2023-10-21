ddeb76cd7cee6079e7d3e463509075d4aede8d9b
Gordon Choi <thefirate@gmail.com>
2023-10-21 16:13:55 +0900

    :tada: TCA Counter 프로젝트 생성

---
+++ TCACounter/oldstar/Counter/Counter/CounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CounterApp.swift
+//  Counter
+//
+//  Created by Gordon Choi on 10/21/23.
+//
+
+import SwiftUI
+
+@main
+struct CounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
