2d5569e04f6c62e1010dadef4004837f111c891b
saltCastle77 <hysung01@gmail.com>
2023-10-22 15:55:21 +0900

    add: 서버 통신을 사용한 속담 불러오기 완료

--- TCA Essential/Counter/TCAEssential/TCAEssential/Reducer/CounterFeature.swift
+++ TCA Essential/Counter/TCAEssential/TCAEssential/Reducer/CounterFeature.swift
@@ -10,25 +10,51 @@ import ComposableArchitecture
 
 struct CounterFeature: Reducer {
     struct State {
-       var count = 0
-     }
-
-     enum Action {
-       case decrementButtonTapped
-       case incrementButtonTapped
-     }
-
+        var count = 0
+        var fact: String?
+        var isLoading = false
+    }
+    
+    enum Action {
+        case decrementButtonTapped
+        case incrementButtonTapped
+        case factResponse(String)
+        case factButtonTapped
+    }
+    
     func reduce(into state: inout State, action: Action) -> Effect<Action> {
         switch action {
+            
         case .decrementButtonTapped:
-          state.count -= 1
-          return .none
-
+            state.count -= 1
+            state.fact = nil
+            return .none
+            
         case .incrementButtonTapped:
-          state.count += 1
-          return .none
+            state.count += 1
+            state.fact = nil
+            return .none
+            
+        case .factButtonTapped:
+            state.fact = nil
+            state.isLoading = true
+            
+            return .run { [count = state.count] send in
+                let (data, _) = try await URLSession.shared
+                    .data(from: URL(string: "http://numbersapi.com/\(count)")!)
+                let fact = String(decoding: data, as: UTF8.self)
+                ///가져온 fact의 값을 state.fact = fact로 바로 변경할 수 없음.
+                ///reduce함수의 stae인자가 inout이기 떄문에 .run이라는 함수안에서 state가 변경되면 원자성을 해치기 때문임.
+                ///그래서 받아온 factRsponse를 await aync를 사용해서 다시 action으로 보내줘야한ㄷ.
+                await send(.factResponse(fact))
+            }
+        case let .factResponse(fact):
+            state.fact = fact
+            state.isLoading = false
+            return .none
         }
-      }
+        
+    }
 }
 
 extension CounterFeature.State: Equatable {
