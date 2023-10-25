49a3021179423d25a5361ec677a596e26b80a3a0
Gordon Choi <thefirate@gmail.com>
2023-10-26 01:03:50 +0900

    :tada: TCA Standups 생성 및 초기 설정

---
+++ TCAStandups/oldstar/Standups/Standups/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Standups
+//
+//  Created by Gordon Choi on 10/26/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TCAStandups/oldstar/Standups/Standups/StandupsApp.swift
@@ -0,0 +1,17 @@
+//
+//  StandupsApp.swift
+//  Standups
+//
+//  Created by Gordon Choi on 10/26/23.
+//
+
+import SwiftUI
+
+@main
+struct StandupsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
